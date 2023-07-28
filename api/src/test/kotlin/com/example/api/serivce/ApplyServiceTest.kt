87fdddf454fae644174c389844ba5077d126f366
kkyu8925 <kkyu8925@gmail.com>
2023-07-28 22:02:00 +0900

    섹션 1. 쿠폰발급로직 race condition

--- api/src/test/kotlin/com/example/api/serivce/ApplyServiceTest.kt
+++ api/src/test/kotlin/com/example/api/serivce/ApplyServiceTest.kt
@@ -5,6 +5,8 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
 
 @SpringBootTest
 class ApplyServiceTes @Autowired constructor(
@@ -19,5 +21,29 @@ class ApplyServiceTes @Autowired constructor(
         val count = couponRepository.count()
 
         assertThat(count).isEqualTo(1)
+    }
+
+    @Test
+    fun 여러명응모() {
+        val threadCount = 1000;
+        val executorService = Executors.newFixedThreadPool(32)
+        val latch = CountDownLatch(threadCount)
+
+        for (i in 0..threadCount) {
+            val userId = i.toLong()
+            executorService.submit {
+                try {
+                    applyService.apply(userId)
+                } finally {
+                    latch.countDown()
+                }
+            }
+        }
+
+        latch.await()
+
+        val count = couponRepository.count()
+
+        assertThat(count).isEqualTo(100)
     }
 }
